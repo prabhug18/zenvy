46ba3b5d9feb9799a6d3e1c5b5258290adc67445
unknown <santhoshkumardkv18@email.com>
2026-04-10 15:42:51 +0530

    Initial commit4 10/04/2026

--- Modules/LMS/app/Repositories/Testimonial/TestimonialRepository.php
+++ Modules/LMS/app/Repositories/Testimonial/TestimonialRepository.php
@@ -20,7 +20,7 @@ class TestimonialRepository extends BaseRepository
         'save' => [
             'name' => 'required|string',
             'designation' => 'required|string',
-            'rating' => 'required|numeric|min:1|max:5',
+            'rating' => 'required',
             'image' => 'required|image|mimes:jpg,jpeg,png,svg,webp',
             'comments' => 'required',
         ],
